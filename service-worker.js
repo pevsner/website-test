987a48adbd6958623006aa805aed299a7e7ee758
pevsner <pevsner@protonmail.com>
2023-04-30 21:31:00 +0100

    New tester files.

--- service-worker.js
+++
@@ -1,6 +0,0 @@
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
-
-workbox.routing.registerRoute{
-    ({request}) => request.destination === 'image',
-    new workbox.strategies.NetworkFirst()
-};
